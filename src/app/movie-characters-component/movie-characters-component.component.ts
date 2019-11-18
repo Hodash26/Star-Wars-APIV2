5ae8c91f12b354c1da30d886f2de7258de1e9d24
Hodash26 <florent.bailly62400@gmail.com>
2019-11-18 12:23:15 +0100

    try to fix characters

--- src/app/movie-characters-component/movie-characters-component.component.ts
+++ src/app/movie-characters-component/movie-characters-component.component.ts
@@ -1,26 +1,26 @@
-import { Component, OnInit, Input} from '@angular/core';
-import { Character } from '../characters'
-import { StarWarsService } from '../star-wars.service';
+import { Component, OnInit, Input } from "@angular/core";
+import { Character } from "../characters";
+import { StarWarsService } from "../star-wars.service";
 
 @Component({
-  selector: 'app-movie-characters-component',
-  templateUrl: './movie-characters-component.component.html',
-  styleUrls: ['./movie-characters-component.component.css']
+  selector: "app-movie-characters-component",
+  templateUrl: "./movie-characters-component.component.html",
+  styleUrls: ["./movie-characters-component.component.css"]
 })
 export class MovieCharactersComponentComponent implements OnInit {
   @Input() url: string;
-characters: Character[];
-selectedPeople: Character;
+  characters: Character[];
+  selectedPeople: Character;
 
-
-  constructor(private starwarsService:StarWarsService) { }
+  constructor(private starwarsService: StarWarsService) {}
 
   ngOnInit() {
     this.getcharacters();
   }
 
   getcharacters(): void {
-    this.starwarsService.getStarWarsCharacters().subscribe(characters => (this.characters=characters));
+    this.starwarsService
+      .getStarWarsCharacters()
+      .subscribe(characters => (this.characters = characters));
   }
-
 }
